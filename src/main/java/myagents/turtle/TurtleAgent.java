7080912c3ebb4140a25cfced97a9f29fbb28c864
pnakibar <pedronakibar@gmail.com>
2015-03-09 20:07:59 -0300

    Update TurtleAgent.java

--- src/main/java/myagents/turtle/TurtleAgent.java
+++ src/main/java/myagents/turtle/TurtleAgent.java
@@ -15,21 +15,19 @@ import jade.lang.acl.MessageTemplate;
 import jade.lang.acl.UnreadableException;
 
 public class TurtleAgent extends Agent{
-	
-	//JADE things
 	private static final long serialVersionUID = 1L;
 	
-	//ROS things
 	private Ros ros;
 	private Topic turtleTopic;
 	
 	
 	
 	public TurtleAgent(){
-		ros = new Ros("localhost");
-	    ros.connect();
-	    turtleTopic = new Topic(ros, "/turtle1/cmd_vel", "geometry_msgs/Twist");
-	    turtleTopic.subscribe(new TopicCallback() {
+		//inicia o ROS com suas configurações
+		ros = new Ros("localhost"); //localização do ROS
+	    	ros.connect(); //conecta ao ROS
+	    	turtleTopic = new Topic(ros, "/turtle1/cmd_vel", "geometry_msgs/Twist"); //define o topico, com o tipo de mensagem e o recipiente
+	    	turtleTopic.subscribe(new TopicCallback() { //assina o tópico e define uma função para ser executada quando for respondidade
 			public void handleMessage(Message arg0) {
 				//do nothing
 			}
@@ -37,7 +35,7 @@ public class TurtleAgent extends Agent{
 	
 	}
 	
-	private void moveTurtle(Vector3 linear, Vector3 angular){
+	private void moveTurtle(Vector3 linear, Vector3 angular){ //envia a mensagem para o ROS
 		Message toSend = new Twist(linear, angular);
 	    turtleTopic.publish(toSend);
 	    try {
@@ -47,7 +45,7 @@ public class TurtleAgent extends Agent{
 			e.printStackTrace();
 		}
 	}
-	private void moveTurtle(Float[] linear, Float[] angular){
+	private void moveTurtle(Float[] linear, Float[] angular){ //envia a mensagem para o ROS
 		Vector3 linearVector = new Vector3(linear[0],linear[1],linear[2]);
 		Vector3 angularVector = new Vector3(angular[0],angular[1],angular[2]);
 		this.moveTurtle(linearVector, angularVector);
@@ -56,19 +54,21 @@ public class TurtleAgent extends Agent{
 	
 	
 	protected void setup(){
-		addBehaviour(new OfferRequestsServer());
+		addBehaviour(new OfferRequestsServer()); //adiciona comportamento ao agente
 		System.out.println("Hello, "+getAID().getName()+" is ready");
 	}
 
-	
+	//classe que define o comportamento de receber mensagens
 	private class OfferRequestsServer extends CyclicBehaviour {
 		@Override
 		public void action() {
-			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
-			ACLMessage msg = myAgent.receive(mt);
+			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);//apenas recebe mensagens do tipo REQUEST
+			ACLMessage msg = myAgent.receive(mt); //recebe a mensagem, remove da pilha
 			if (msg != null){
 				try {
-					Float[][] content = (Float[][]) msg.getContentObject();
+					//deserializa o conteúdo da mensagem
+					Float[][] content = (Float[][]) msg.getContentObject(); 
+					//chama o metódo de enviar mensagem para o ROS
 					moveTurtle(new Vector3(content[0][0],content[0][1],content[0][2]), new Vector3(content[1][0],content[1][1],content[1][2]));
 				} catch (UnreadableException e) {
 					e.printStackTrace();
